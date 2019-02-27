4f27a5f1c2fbb21dac74e0b0e48529ff07b04cd5
Sayanna Chandula <sayanna@gmail.com>
2019-02-26 21:25:50 -0800

    implement test code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,69 @@
 #include <iostream>
+#include "vector-op.hpp"
+using namespace std;
+
+#define dbglog printf
+#define msglog printf
+#define errlog printf
+
+static void _testScaleVector() {
+	mathVector v1(10,12);
+	double scale = 12.0;
+
+	mathVector res = scaleVector(v1,scale);
+
+	msglog("v1 = [%f,%f]\n",v1.x,v1.y);
+	msglog("res = [%f,%f]\n",res.x,res.y);
+}
+
+static void _testSubVectors() {
+	mathVector v1(10,12);
+	mathVector v2(11,21);
+
+	mathVector res = substractVectors(v1,v2);
+
+	msglog("v1 = [%f,%f]\n",v1.x,v1.y);
+	msglog("v2 = [%f,%f]\n",v2.x,v2.y);
+	msglog("res = [%f,%f]\n",res.x,res.y);
+}
+
+static void _testAddVectors() {
+	mathVector v1(10,12);
+	mathVector v2(11,21);
+
+	mathVector res = addVectors(v1,v2);
+
+	msglog("v1 = [%f,%f]\n",v1.x,v1.y);
+	msglog("v2 = [%f,%f]\n",v2.x,v2.y);
+	msglog("res = [%f,%f]\n",res.x,res.y);
+}
+
+static void printUsage() {
+	msglog("Usage : ./test <command>\n");
+	msglog("testAddVectors\n");
+	msglog("testSubVectors\n");
+	msglog("testScaleVector\n");
+}
 
 int main(int argc, char **argv)
 {
-	std::cout << "Hello world" << std::endl;
+	if (argc < 2) {
+		printUsage();
+		return 0;
+	}
+
+	string command = argv[1];
+
+	if (command == "testAddVectors") {
+		_testAddVectors();
+	} else if (command == "testSubVectors") {
+		_testSubVectors();
+	} else if (command == "testScaleVector") {
+		_testScaleVector();
+	} else {
+		cout << "Invalid command\n";
+		printUsage();
+	}
+
+	return 0;
 }
--- vector-op.hpp
+++ vector-op.hpp
@@ -4,6 +4,7 @@ typedef struct _mathVector{
 	double x;
 	double y;
 	_mathVector():x(0),y(0){};
+	_mathVector(double a, double b):x(a),y(b){};
 } mathVector;
 
 mathVector addVectors(mathVector v1, mathVector v2);
